2a89fd26e00e08270939e144ef1226960acd6e0a
MMGGDH <tldmseogus@naver.com>
2023-07-26 17:54:33 +0900

    셋팅 완료

--- src/main/java/shop/mtcoding/blog/controller/UserController.java
+++ src/main/java/shop/mtcoding/blog/controller/UserController.java
@@ -7,6 +7,14 @@ import org.springframework.web.bind.annotation.PostMapping;
 @Controller
 public class UserController {
 
+    // Get요청을 하는 방법
+    // 1. a태그 (하이퍼 링크)
+    // 2. form태그의 get메서드를 호출
+
+    // get메서드를 호출하려면 요청이 ip주소와 포트번호를 타고 와야함
+    // ip주소와 포트 부여: 10.5.9.200:8080 -> 불편하니까 도메인주소(DNS, mtcoding.com:8080)를 구매
+    // 이때 포트번호가 80이면 생략가능
+    // 자기 ip를 적으면 나갔다 다시 돌아온다 << 그래서 다시 나갔다 다시 돌아올 바에 루프백주소를 쓰면 된다
     @GetMapping({ "/joinForm" })
     public String joinForm() {
         return "user/joinForm";
