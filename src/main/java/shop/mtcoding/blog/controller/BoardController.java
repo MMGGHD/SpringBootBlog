ca06d2b580b841c5303e880b748dd8d83397427d
MMGGDH <tldmseogus@naver.com>
2023-08-02 12:46:50 +0900

    상세보기 완료

--- src/main/java/shop/mtcoding/blog/controller/BoardController.java
+++ src/main/java/shop/mtcoding/blog/controller/BoardController.java
@@ -12,7 +12,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.Mapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import net.bytebuddy.asm.MemberSubstitution.Substitution.Chain.Step.ForField.Write;
 import shop.mtcoding.blog.dto.WriteDTO;
@@ -29,24 +31,57 @@ public class BoardController {
     @Autowired
     private BoardRepository boardRepository;
 
+    // <스프링에서 쿼리스트링 자동 파싱의 예시>
+    // restful API << 주소설계 프로토콜(주소를 사람이봐도 알수 있게 짜야 한다)
+    // restful API 조건 변수를 받는 방식 2가지 1) @PathVariable, 2)쿼리스트링
+    // 1) @PathVariable 조건은 무조건 PK또는 UK를 찾는다. (프로토콜)
+    // 2) 쿼리스트링 << 유일값이 들어가면 안된다. (프로토콜)
+    // 쿼리스트링 = 구체적 질의라는 뜻, Where절을 받기 위해서 사용된다.
+    // 쿼리스트링 요청이 오면 스프링이 메서드의 매개변수로 자동으로 파싱해준다.
+    // 예를들어 localhost:8080?a=1&b=50 이라는 URL요청이 들어온다면
+    // 스프링이 아래 hello메서드 매개변수(a,b)안에 ?a=1&b=50을 파싱해서 1,50을 넣어준다.
+    // 매개변수가 Wrapper클래스라면 쿼리스트링이 없어도 null값으로 입력된다.
+
+    @ResponseBody
+    @GetMapping("/hello")
+    public String hello(Integer a, Integer b) {
+        System.out.println("a : " + a);
+        System.out.println("b : " + b);
+        return "ok";
+    }
+
     // 유효성 검사 필요없음
     // 인증 검사 필요없음
+    // localhost:8080?page=1
     // @RequestParam << 값이 들어오지 않으면 기본값을 설정할수 있는 어노테이션
     // @RequestParam(defaultValue = "0") Integer page << page에 값 안들어오면 "0" 대입됨
     @GetMapping({ "/", "/board" })
     public String index(@RequestParam(defaultValue = "0") Integer page, HttpServletRequest request) {
 
         List<Board> boardList = boardRepository.findAll(page);
+
+        // Last 계산하는 과정 (복잡하니 다른메서드로 빼는것이 낫다)
+
+        // LastPage는 DB에서 카운트 연산해야함 (boardRepository.count())
+        int totalCount = boardRepository.count();
+        System.out.println("totalCount : " + totalCount);
+        int totalPage = totalCount / 3;
+        if (totalCount % 3 > 0) {
+            totalPage = totalPage + 1;
+        }
+        boolean last = totalPage - 1 == page;
+
         System.out.println("테스트 : " + boardList.size());
         System.out.println("테스트 : " + boardList.get(0).getTitle());
 
+        // 아래처럼 페이지를 위한 데이터를 페이징 데이터라고 한다.
         request.setAttribute("boardList", boardList);
         request.setAttribute("prevPage", page - 1);
         request.setAttribute("nextPage", page + 1);
         request.setAttribute("first", page == 0 ? true : false);
-
-        // LastPage는 DB에서 카운트 연산해야함
-        request.setAttribute("last", false);
+        request.setAttribute("last", last);
+        request.setAttribute("totalPage", totalPage);
+        request.setAttribute("totalCount", totalCount);
 
         return "index";
     }
@@ -83,12 +118,19 @@ public class BoardController {
         return "redirect:/";
     }
 
-    // localhost:/board/1
-    // localhost:/board/50
+    // 권한체크가 필요함
     @GetMapping({ "/board/{id}" })
-    public String detail(@PathVariable Integer id, Model model) {
-        Board board = boardRepository.findBoardById(id);
-        model.addAttribute("board", board);
+    public String detail(@PathVariable Integer id, HttpServletRequest request) {
+
+        User sessionUser = (User) session.getAttribute("sessionUser"); // 세션접근
+        Board board = boardRepository.findById(id);
+        boolean pageOwner = false;
+        if (sessionUser != null) {
+            pageOwner = sessionUser.getId() == board.getUser().getId();
+        }
+
+        request.setAttribute("board", board);
+        request.setAttribute("pageOwner", pageOwner);
         return "board/detail";
     }
 
--- src/main/java/shop/mtcoding/blog/repository/BoardRepository.java
+++ src/main/java/shop/mtcoding/blog/repository/BoardRepository.java
@@ -1,5 +1,6 @@
 package shop.mtcoding.blog.repository;
 
+import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -22,6 +23,19 @@ public class BoardRepository {
     @Autowired
     private EntityManager em;
 
+    // select id, title from board_tb
+    // resultClass 안붙이고 직접 파싱하려면 Obgject 배열(Object[])로 리턴된 값을 파싱
+    // object[0] = 1
+    // object[1] = 제목1
+    public int count() {
+        Query query = em.createNativeQuery("select count(*) from board_tb");
+        // 원래는 Object 배열로 리턴을 받는다, Obgject 배열은 칼럼값의 연속이다.
+        // 그룹함수를 써서, 하나의 칼럼을 조회하면 Obgject로 리턴된다.
+        BigInteger count = (BigInteger) query.getSingleResult();
+        return count.intValue();
+
+    }
+
     @Transactional
     public void save(WriteDTO writeDTO, Integer userId) {
         System.out.println("save Repository 메서드 실행");
@@ -37,7 +51,8 @@ public class BoardRepository {
     // 즉, 객체지향 쿼리를 쓰면 DBMS마다 다른 문법을 신경쓰지 않고 자바에서 쓸수 있다.
     public List<Board> findAll(int page) {
         // 페이징을 해서 조회한다. 페이징쿼리는 DB에서 먼저 해보고 작성한다.
-        // limit int a, int b << a는 시작페이지, b 는 페이지당 표시할 갯수
+        // [limit int a, int b] << a는 시작인덱스 번호, b는 표시할 갯수
+        // 예를 들어 limit 1,3 이면 DB의 1, 2, 3인덱스를 가진 튜플만 검색된다.
         // limit은 쿼리문의 가장 뒤에 들어가야 한다.
         // 페이지당 표시할 갯수는 변하면 안되기 때문에 final int로 지정한다.
         final int SIZE = 3;
@@ -48,14 +63,11 @@ public class BoardRepository {
         return query.getResultList();
     }
 
-    public Board findBoardById(Integer id) {
-        System.out.println("findBoardById 메서드 실행");
+    public Board findById(Integer id) {
         Query query = em.createNativeQuery(
                 "select * from board_tb where id =:id", Board.class);
-        System.out.println("findBoardById 쿼리입력");
         query.setParameter("id", id);
         Board board = (Board) query.getSingleResult();
-        System.out.println("findBoardById 객체 전달");
         return board;
     }
 }
